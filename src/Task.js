8ed56ce692fe765f48bc4fdf50728d6efdaf7b0c
aldimhndr <aldimhndr201@gmail.com>
2022-12-23 15:38:51 +0700

    Add project files to Git

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://final-project-deploy-c9d4f.web.app" // TODO: replace this
+const studentName = "Aldi Mahendra" // TODO: replace this
+const studentId = "FE01" // TODO: replace this
+
+export { url, studentName, studentId }
